b84745462c0a34fe965606d1a46a5199586c3ff9
Miguel Sanchez <smiguel706@gmail.com>
2019-07-26 12:37:07 -0500

    funciones nuevas en matrices.h

---
+++ matrices.h
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+int lec_M(int m, int n, double A[][40]);
+int esc_M(int m, int n, double A[][40]);
+int prod_AB(int m, int n, int p, int q, double A[][40], double B[][40], double C[][40]);
+
+int lec_M(int m, int n, double A[][40]) {
+	int i,j;
+	if(n <= 0 || n > 40 || m <= 0 || m > 40) return 0;
+	for(i = 0; i < m; i++) {
+		for(j = 0; j < n; j++) {
+			scanf("%lf", &A[i][j]);
+		}
+	}
+	
+	return 1;
+}
+
+int esc_M(int m, int n, double A[][40]) {
+	int i,j;
+	if(n <= 0 || n > 40 || m <= 0 || m > 40) {
+		printf("Argumentos no validos");
+		return 0;
+	}
+	for(i = 0; i < m; i++) {
+		for(j = 0; j < n; j++) {
+			printf(" %.2lf ", A[i][j]);
+		}
+		printf("\n");
+	}
+	puts("\n");
+	return 1;
+}
+
+int prod_AB(int m, int n, int p, int q, double A[][40], double B[][40], double C[][40]) {
+	if (m<=0 || n<=0 || p<=0 || q<=0 || m>40 || n>40 || p>40 || q>40 || n!=p) return 0;
+	
+	int i,j,k;
+	double s;
+	
+	for(i = 0; i < m; i++) {
+		for (j = 0;j < q; j++) {
+			s = 0.0;
+			for (k = 0; k < n; k++)	s += A[i][k]*B[k][j];
+			C[i][j] = s;
+		}
+	}
+	
+	return 1;
+}
+
+
