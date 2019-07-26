949f51b1b90ad38bd32e47df4af8836731f25553
Miguel Sanchez <smiguel706@gmail.com>
2019-07-26 15:06:40 -0500

    se utilizan punteros en las funciones de escritura y lectura de matrices en matrices.h

--- matrices.h
+++ matrices.h
@@ -6,28 +6,33 @@ int prod_AB(int m, int n, int p, int q, double A[][40], double B[][40], double C
 
 int lec_M(int m, int n, double A[][40]) {
 	int i,j;
+	double *p;
+	p = &A[0][0];
 	if(n <= 0 || n > 40 || m <= 0 || m > 40) return 0;
 	for(i = 0; i < m; i++) {
-		for(j = 0; j < n; j++) {
-			scanf("%lf", &A[i][j]);
-		}
+		for(j = 0; j < n; j++) scanf("%lf", p++);
+		p = p+40-j;
 	}
 	
 	return 1;
 }
 
 int esc_M(int m, int n, double A[][40]) {
-	int i,j;
 	if(n <= 0 || n > 40 || m <= 0 || m > 40) {
 		printf("Argumentos no validos");
 		return 0;
 	}
+	
+	int i,j;
+	double *p;
+	p = &A[0][0];
+	
 	for(i = 0; i < m; i++) {
-		for(j = 0; j < n; j++) {
-			printf(" %.2lf ", A[i][j]);
-		}
+		for(j = 0; j < n; j++) 	printf(" %.2lf ", *p++);
+		p = p+40-j;
 		printf("\n");
 	}
+	
 	puts("\n");
 	return 1;
 }
