c3d7906e4edfb601673881d23c9529c0d6eb35d2
Miguel Sanchez <smiguel706@gmail.com>
2019-08-01 08:34:50 -0500

    Se agrego una estructura llamada matrices para manejar arreglos unidimencionales como matrices y se modificaron las funciones

--- matrices.h
+++ matrices.h
@@ -1,61 +1,123 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int lec_M(int m, int n, double A[][40]);
-int esc_M(int m, int n, double A[][40]);
-int prod_AB(int m, int n, int p, int q, double A[][40], double B[][40], double C[][40]);
+struct matriz {
+	double *mt;
+	int m,n;
+	int listo,llena;
+};
 
-int lec_M(int m, int n, double A[][40]) {
-	int i,j;
-	double *p;
-	p = &A[0][0];
-	if(n <= 0 || n > 40 || m <= 0 || m > 40) return 0;
-	for(i = 0; i < m; i++) {
-		for(j = 0; j < n; j++) scanf("%lf", p++);
-		p += 40-j;
+int asignar_matriz(struct matriz *p, int m1, int n1);
+int lec_M(struct matriz *p);
+int esc_M(struct matriz *p);
+int prod_AB1(struct matriz A,struct matriz B,struct matriz *C);
+
+
+int asignar_matriz(struct matriz *p, int m1, int n1) {
+	if(n1 > 0 && m1 > 0){
+		p->mt = (double *) malloc(m1*n1*sizeof(double));
+		if(p->mt != NULL) {
+			p->m = m1;
+			p->n = n1;
+			p->llena = 0;
+			p->listo = 1;
+			return 1;
+		}
 	}
+	p->listo = 0;
+	return 0;
 	
-	return 1;
 }
 
-int esc_M(int m, int n, double A[][40]) {
-	if(n <= 0 || n > 40 || m <= 0 || m > 40) {
-		printf("Argumentos no validos");
+int lec_M(struct matriz *p) {
+	if(p->listo) {
+		int i,j;
+		double *mr;
+		mr = p->mt;
+		for(i = 0; i < p->m; i++) {
+			for(j = 0; j < p->n; j++) scanf("%lf", mr++);
+		}	
+		p->llena = 1;
+		return 1;
+	}
+	
+	return 0;
+}
+
+int esc_M(struct matriz *p) {
+	if(p->listo && p->llena) {
+		int i,j;
+		double *mr;
+		mr = p->mt;
+		for(i = 0; i < p->m; i++) {
+			for(j = 0; j < p->n; j++) 	printf(" %.2lf ",*mr++);
+			printf("\n");
+		}
+		return 1;
+	}
+
+	return 0;
+}
+
+int prod_AB1(struct matriz A,struct matriz B,struct matriz *C){
+	if(A.listo && A.llena && B.listo && B.llena && A.n == B.m) {
+		asignar_matriz(C,A.m,B.n);
+		if(C->listo) {
+			double *cij,*cmq,*ciq,*boj,*aio,*ain,*aik,*bkj;
+			int m,q,n;
+			n = A.n;
+			m = A.m;
+			q = B.n;
+			cij = C->mt;
+			printf("%p\n",cij);
+			cmq = cij +m*q;
+			aio = A.mt;
+			
+			while(cij < cmq) {
+				ciq = cij+q;
+				boj = B.mt;
+				while(cij < ciq) {
+					aik = aio;
+					ain = aio + n;
+					bkj = boj;
+					*cij = 0.0;
+					while(aik < ain) {
+						*cij += (*aik++)*(*bkj);
+						bkj += q;
+					}
+					cij++;
+					boj++;
+				}
+				aio = ain;
+			}
+			
+			C->llena = 1;
+			return 1;
+		}
 		return 0;
 	}
-	
-	int i,j;
-	double *p;
-	p = &A[0][0];
-	
-	for(i = 0; i < m; i++) {
-		for(j = 0; j < n; j++) 	printf(" %.2lf ", *p++);
-		p += 40-j;
-		printf("\n");
-	}
-	
-	puts("\n");
-	return 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-int prod_AB(int m, int n, int p, int q, double A[][40], double B[][40], double C[][40]) {
-	if (m<=0 || n<=0 || p<=0 || q<=0 || m>40 || n>40 || p>40 || q>40 || n!=p) return 0;
-	
-	int i,j,k;
-	double s,*pa,*pb,*pc;
-	pa = &A[0][0];
-	pb = &B[0][0];
-	pc = &C[0][0];
-	
-	for(i = 0; i < m; i++) {
-		for (j = 0;j < q; j++) {
-			s = 0.0;
-			for (k = 0; k < n; k++)	s += (*(pa+i*40+k))*(*(pb+k*40+j));
-			*pc++ = s;
-		}
-		pc+= 40-j;
-	}
-	
-	return 1;
-}
+
+
 
 
